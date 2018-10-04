7e54b606c621265035ea4b8262806dfff21f096b
gabiquichiz93 <gabrielaquichiz@hotmail.com.ar>
2018-10-03 21:19:02 -0300

    agrego admin general y el usuario default para ingresar al sistema

--- BaseDeDatos/ScriptsMigracion/creacionRoles.sql
+++ BaseDeDatos/ScriptsMigracion/creacionRoles.sql
@@ -4,3 +4,9 @@ INSERT INTO [LOOPP].[Roles]([nombre])
 VALUES ('Cliente');
 INSERT INTO [LOOPP].[Roles]([nombre])
 VALUES ('Empresa');
+INSERT INTO [LOOPP].[Roles]([nombre])
+VALUES ('Administrador General');
+
+INSERT INTO [LOOPP].[Usuarios] (username,password)
+--user :admin pass: w23e
+VALUES ('admin', '52d77462b24987175c8d7dab901a5967e927ffc8d0b6e4a234e07a4aec5e3724');
