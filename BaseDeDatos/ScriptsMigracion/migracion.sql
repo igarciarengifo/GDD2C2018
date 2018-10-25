c185f799ce4d30e7783998282cc1c68533570523
angemendez89 <angemendez89@gmail.com>
2018-10-24 22:21:38 -0300

    Agrego al script migracion, la migracion de clientes

--- BaseDeDatos/ScriptsMigracion/migracion.sql
+++ BaseDeDatos/ScriptsMigracion/migracion.sql
@@ -53,9 +53,9 @@ INSERT INTO [LOOPP].[Roles]([nombre]) VALUES ('Cliente');
 INSERT INTO [LOOPP].[Roles]([nombre]) VALUES ('Empresa');
 
 /*Creacion de usuario Admin*/
-INSERT INTO [LOOPP].[Usuarios] (username,password,estado)
+INSERT INTO [LOOPP].[Usuarios] (username,password)
 --user :admin pass: w23e
-VALUES ('admin', '52d77462b24987175c8d7dab901a5967e927ffc8d0b6e4a234e07a4aec5e3724','Habilitado');
+VALUES ('admin', '52d77462b24987175c8d7dab901a5967e927ffc8d0b6e4a234e07a4aec5e3724');
 
 /*Creacion de Rol_X_Usuario para el admin*/
 INSERT INTO [LOOPP].[Rol_X_Usuario] (id_usuario,id_rol) VALUES (1,1);
@@ -106,3 +106,139 @@ SELECT
 FROM gd_esquema.Maestra
 GROUP BY Ubicacion_Fila, Ubicacion_Asiento, Ubicacion_Sin_numerar, Ubicacion_Tipo_Codigo
 ORDER BY Ubicacion_Tipo_Codigo, Ubicacion_Fila, Ubicacion_Asiento
+
+/*Migracion de clientes*/
+
+/*Se agrupa clientes de la tabla Maestra y se inserta en una tabla Temporal*/
+select [Cli_Nombre] nombre
+	  ,[Cli_Apeliido] apellido
+	  ,[Cli_Dni] dni
+      ,[Cli_Fecha_Nac]
+      ,[Cli_Mail] email
+      ,[Cli_Dom_Calle]
+      ,[Cli_Nro_Calle]
+      ,[Cli_Piso]
+      ,[Cli_Depto]
+      ,[Cli_Cod_Postal]
+into #Temp_Clientes
+from [GD2C2018].[gd_esquema].[Maestra]
+where Cli_Dni is not null
+group by [Cli_Nombre]
+	  ,[Cli_Apeliido]
+	  ,[Cli_Dni]
+      ,[Cli_Fecha_Nac]
+      ,[Cli_Mail]
+      ,[Cli_Dom_Calle]
+      ,[Cli_Nro_Calle]
+      ,[Cli_Piso]
+      ,[Cli_Depto]
+      ,[Cli_Cod_Postal];
+/*Se busca si existe incosistencia en los datos mediante otra tabla Temporal*/
+SELECT  [Nombre]
+		 ,[Apellido]
+		 ,[Dni]
+		 ,[Cli_Fecha_Nac]
+		 ,[Email]
+		 ,[Cli_Dom_Calle]
+		 ,[Cli_Nro_Calle]
+		 ,[Cli_Piso]
+		 ,[Cli_Depto]
+		 ,[Cli_Cod_Postal]
+		 ,row_number() OVER(PARTITION BY [Dni] ORDER BY Email) AS cantDni
+		 ,row_number() OVER(PARTITION BY [Email] ORDER BY Email) AS cantEmail
+INTO #Temp_Cli_Incons
+FROM #Temp_Clientes
+GROUP BY	 [Nombre]
+			,[Apellido]
+			,[Dni]
+			,[Cli_Fecha_Nac]
+			,[Email]
+			,[Cli_Dom_Calle]
+			,[Cli_Nro_Calle]
+			,[Cli_Piso]
+			,[Cli_Depto]
+			,[Cli_Cod_Postal];
+
+DROP TABLE #Temp_Clientes;
+
+/*Se inserta tabla usuarios antes de insertar el cliente, ya que cliente tiene un FK a la tabla usuarios*/
+insert into [LOOPP].[Usuarios] (
+		[username]
+		,[password])
+select left(email,charindex('@',email,1)-1) userName
+		,'1234' pass
+from #Temp_Cli_Incons
+where cantDni=1 and cantEmail=1
+/*Se inserta tabla clientes con los datos de clientes que sean unicos*/
+insert into [LOOPP].[Clientes] (
+	   [nombre]
+      ,[apellido]
+      ,[nro_documento]
+      ,[fecha_nacimiento]
+      ,[mail]
+      ,[direccion_calle]
+      ,[direccion_nro]
+      ,[direccion_piso]
+      ,[direccion_depto]
+      ,[codigo_postal]
+      ,[id_usuario] )
+select [Nombre]
+		 ,[Apellido]
+		 ,[Dni]
+		 ,[Cli_Fecha_Nac]
+		 ,[Email]
+		 ,[Cli_Dom_Calle]
+		 ,[Cli_Nro_Calle]
+		 ,[Cli_Piso]
+		 ,[Cli_Depto]
+		 ,[Cli_Cod_Postal]
+		 ,usu.id_usuario
+from #Temp_Cli_Incons tmp
+inner join [LOOPP].[Usuarios] usu
+on left(email,charindex('@',email,1)-1)=usu.username
+where cantDni=1 and cantEmail=1
+order by dni
+
+/*Se inserta tabla usuarios antes de insertar el cliente, ya que cliente tiene un FK a la tabla usuarios*/
+insert into [LOOPP].[Usuarios] (
+		[username]
+		,[password])
+select left(email,charindex('@',email,1)-1)+'_duplicado' userName
+		,'1234' pass
+from #Temp_Cli_Incons
+where cantDni=1 and cantEmail>1
+
+/*Se inserta tabla clientes con los datos de clientes que poseen datos incosistentes
+  Como por ejemplo 2 emials iguales para clientes con distinto dni*/
+insert into [LOOPP].[Clientes] (
+	   [nombre]
+      ,[apellido]
+      ,[nro_documento]
+      ,[fecha_nacimiento]
+      ,[mail]
+      ,[direccion_calle]
+      ,[direccion_nro]
+      ,[direccion_piso]
+      ,[direccion_depto]
+      ,[codigo_postal]
+	  ,[estado]
+      ,[id_usuario] )
+select [Nombre]
+		 ,[Apellido]
+		 ,[Dni]
+		 ,[Cli_Fecha_Nac]
+		 ,[Email]
+		 ,[Cli_Dom_Calle]
+		 ,[Cli_Nro_Calle]
+		 ,[Cli_Piso]
+		 ,[Cli_Depto]
+		 ,[Cli_Cod_Postal]
+		 ,'Inconsistente' estado
+		 ,usu.id_usuario
+from #Temp_Cli_Incons tmp
+inner join [LOOPP].[Usuarios] usu
+on left(email,charindex('@',email,1)-1)+'_duplicado'=usu.username
+where cantDni=1 and cantEmail>1
+order by dni
+
+DROP TABLE #Temp_Cli_Incons;
