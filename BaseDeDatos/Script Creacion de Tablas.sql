e791c8ecbfdb1279b07692e1b4bc51c11eeb6a23
DanielaRios <daniela_erre@hotmail.com>
2018-11-11 13:58:53 -0300

    .

--- BaseDeDatos/Script Creacion de Tablas.sql
+++ BaseDeDatos/Script Creacion de Tablas.sql
@@ -188,6 +188,7 @@ Print '***Inicio de creacion de tablas***'
 		[id_ubicacion] [int] NOT NULL,
 		[precio] numeric(18,2) NOT null,
 		[fecha_espectaculo] date not null,
+		[fecha_Venc_espectaculo] date not null,
 		[hora_espectaculo] time not null,
 		PRIMARY KEY ([id_espectaculo], [id_ubicacion]),
 		foreign key ([id_espectaculo]) references [LOOPP].[Espectaculos]([id_espectaculo]),
