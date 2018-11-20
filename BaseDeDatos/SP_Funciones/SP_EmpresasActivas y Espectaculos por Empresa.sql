3c6f733b686897600d5af9e7f36f097b1f7cb050
angemendez89 <angemendez89@gmail.com>
2018-11-19 23:17:08 -0300

    Subo nuevo SP

---
+++ BaseDeDatos/SP_Funciones/SP_EmpresasActivas y Espectaculos por Empresa.sql
@@ -0,0 +1,31 @@
+/*SP que devuelve todas las empresas activas*/
+IF OBJECT_ID('[LOOPP].[SP_AllEmpresasActivas]') IS NOT NULL
+    DROP PROCEDURE [LOOPP].[SP_AllEmpresasActivas]
+GO
+CREATE PROCEDURE [LOOPP].[SP_AllEmpresasActivas]
+AS
+BEGIN
+	select [id_empresa],[razon_social]
+	from [LOOPP].[Empresas]
+	where [baja_logica] = 'False'
+
+END
+GO
+
+/*SP que devuelve todos los espectaculos de una empresa*/
+IF OBJECT_ID('[LOOPP].[SP_AllEspectaculosPorIdEmpresa]') IS NOT NULL
+    DROP PROCEDURE [LOOPP].[SP_AllEspectaculosPorIdEmpresa]
+GO
+CREATE PROCEDURE [LOOPP].[SP_AllEspectaculosPorIdEmpresa] @idEmpresa int
+AS
+BEGIN
+	select [id_espectaculo],[descripcion]
+	from [LOOPP].[Empresas] emp
+	inner join [LOOPP].[Usuarios] usu
+	on emp.id_usuario=usu.id_usuario and emp.id_empresa=@idEmpresa
+	inner join [LOOPP].[Espectaculos] esp
+	on usu.id_usuario=esp.id_usuario_responsable
+	group by [id_espectaculo],[descripcion]
+
+END
+GO
