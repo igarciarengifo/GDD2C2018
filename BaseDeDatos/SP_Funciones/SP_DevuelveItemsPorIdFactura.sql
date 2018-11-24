235ff6f7d44880ab1f1d9155b3608a91d81a3230
angemendez89 <angemendez89@gmail.com>
2018-11-24 16:49:02 -0300

    Corrijo SP

--- BaseDeDatos/SP_Funciones/SP_DevuelveItemsPorIdFactura.sql
+++ BaseDeDatos/SP_Funciones/SP_DevuelveItemsPorIdFactura.sql
@@ -1,10 +1,28 @@
-create PROCEDURE [LOOPP].[SP_DevuelveItemsPorIdFactura] @idFactura int
+/*    ==Scripting Parameters==
+
+    Source Server Version : SQL Server 2016 (13.0.4224)
+    Source Database Engine Edition : Microsoft SQL Server Express Edition
+    Source Database Engine Type : Standalone SQL Server
+
+    Target Server Version : SQL Server 2017
+    Target Database Engine Edition : Microsoft SQL Server Standard Edition
+    Target Database Engine Type : Standalone SQL Server
+*/
+
+USE [GD2C2018]
+GO
+/****** Object:  StoredProcedure [LOOPP].[SP_DevuelveItemsPorIdFactura]    Script Date: 24/11/2018 4:40:56 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [LOOPP].[SP_DevuelveItemsPorIdFactura] @idFactura int
 AS
 	select 		  comp.fecha_compra
 				  ,tub.descripcion+' - Fila '+ub.fila+' - Asiento '+cast(ub.asiento as varchar(10))+' '+case when ub.sin_numerar=1 then 'Sin numero' else '' end ubicacion
 				  ,sum(comp.importe_total) importe_total
 				  ,sum(comp.cantidad_compra) cantidad_compra
-				  ,[LOOPP].[Fn_CalcularComision](comp.importe_total,esp.id_grado_publicacion) comision
+				  ,SUM([LOOPP].[Fn_CalcularComision](comp.importe_total,esp.id_grado_publicacion)) comision
 			from [LOOPP].[Item_Factura] ifact
 			left join [LOOPP].[Facturas] fact
 				on ifact.nro_factura=fact.nro_factura
@@ -20,8 +38,7 @@ AS
 				on uesp.id_espectaculo=lv.id_espectaculo
 			inner join [LOOPP].[Compras] comp
 				on lv.id_compra=comp.id_compra
-			where ifact.nro_factura=129119--@idFactura
+			where ifact.nro_factura = @idFactura
 			group by comp.fecha_compra
 					,tub.descripcion+' - Fila '+ub.fila+' - Asiento '+cast(ub.asiento as varchar(10))+' '+case when ub.sin_numerar=1 then 'Sin numero' else '' end
-					,[LOOPP].[Fn_CalcularComision](comp.importe_total,esp.id_grado_publicacion)
 GO
