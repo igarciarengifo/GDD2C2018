99a238a96bfee5828c59ecde0c58d948b0762226
angemendez89 <angemendez89@gmail.com>
2018-11-27 21:42:59 -0300

    Agrego sp

---
+++ BaseDeDatos/ScriptsMigracion/SP_AltaNuevoRol.sql
@@ -0,0 +1,19 @@
+
+IF OBJECT_ID('[LOOPP].[SP_AltaNuevoRol]') IS NOT NULL
+    DROP PROCEDURE [LOOPP].[SP_AltaNuevoRol]
+GO
+CREATE PROCEDURE [LOOPP].[SP_AltaNuevoRol] @nombre varchar(50)
+AS
+	declare @resultado varchar(10)
+	if not exists (select 1 from [LOOPP].[Roles] where nombre=@nombre)
+		BEGIN
+			insert into [LOOPP].[Roles] (nombre)
+			values (@nombre)
+
+			set @resultado='OK'
+		END
+	else set @resultado = 'ERROR'
+
+	select @resultado;
+
+GO
