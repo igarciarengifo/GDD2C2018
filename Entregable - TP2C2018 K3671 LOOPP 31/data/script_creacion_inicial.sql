ce8844d32d0f5982e4b2e9ee7e7651356376ce61
ingridGrc <ivonne.grc20@gmail.com>
2018-12-20 23:26:22 -0300

    Nuevo sp cliente

--- Entregable - TP2C2018 K3671 LOOPP 31/data/script_creacion_inicial.sql
+++ Entregable - TP2C2018 K3671 LOOPP 31/data/script_creacion_inicial.sql
@@ -3016,3 +3016,19 @@ BEGIN
 	SELECT @resultado
 END
 GO
+
+--------------------------------------------------------------------------------
+
+/* LOOPP.SP_GetClientePorIdUsuario */
+
+IF OBJECT_ID('LOOPP.SP_GetClientePorIdUsuario') IS NOT NULL
+	DROP PROCEDURE [LOOPP].SP_GetClientePorIdUsuario;
+GO
+CREATE PROCEDURE [LOOPP].SP_GetClientePorIdUsuario
+	
+	@idUsuario int
+AS
+BEGIN
+	SELECT * FROM Clientes WHERE id_usuario=@idUsuario
+END
+GO
